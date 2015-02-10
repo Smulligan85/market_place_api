2017e3d507cdccd7cdcde77dcf4ae1fd362c2292
Sean Mulligan <sean.mulligan85@gmail.com>
2015-02-10 06:51:47 -0800

    Adding routes fix

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module MarketPlaceApi
   class Application < Rails::Application
@@ -27,13 +24,8 @@ module MarketPlaceApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-  end
-end
-
-# don't generate RSpec tests for views and helpers
+    
+  # don't generate RSpec tests for views and helpers
   config.generators do |g|
     g.test_framework :rspec, fixture: true
     g.fixture_replacement :factory_girl, dir: 'spec/factories'
@@ -44,4 +36,6 @@ end
     g.helper = false
   end
 
-  config.autoload_paths += %W(\#{config.root}/lib)
+  config.autoload_paths += %W(#{config.root}/lib)
+  end
+end
